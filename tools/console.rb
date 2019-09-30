a12e3f7a3d8731fcca78f680d72e843d1d5ea0a4
josieyao <yao.josephine@yahoo.com>
2019-09-30 00:04:08 -0500

    Done

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+l1 = Listing.new("Seattle")
+l2 = Listing.new("Los Angeles")
+l3 = Listing.new("Austin")
+
+g1 = Guest.new("Vidhi")
+g2 = Guest.new("Raul")
+g3 = Guest.new("Josie")
+
+t1 = Trip.new(l1, g2)
+t2 = Trip.new(l2, g3)
+t3 = Trip.new(l3, g1)
+t4 = Trip.new(l3, g3)
+
 binding.pry
 0
